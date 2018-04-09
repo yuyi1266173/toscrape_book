de377c03b34bf5d76250c98dd6e0c4c69b6dd48e
yuyi1266173 <yuyi1266173@163.com>
2018-04-10 00:36:57 +0800

    add BookItem

--- toscrapy_book/items.py
+++ toscrapy_book/items.py
@@ -8,7 +8,12 @@
 import scrapy
 
 
-class ToscrapyBookItem(scrapy.Item):
+class BookItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
-    pass
+    name = scrapy.Field()
+    price = scrapy.Field()
+    review_rating = scrapy.Field()       #评价等级，1-5星
+    review_num = scrapy.Field()          #评价数量
+    upc = scrapy.Field()                 #产品编码
+    stock = scrapy.Field()               #库存量
